814c72667c0d09b4e805c4f77e2fc277c94fd092
TellSamm <chernikov.semen21@gmail.com>
2025-11-20 22:26:31 +0300

    -- fix dev_v2

--- internal/uploader/client.go
+++ internal/uploader/client.go
@@ -6,6 +6,7 @@ import (
 	"mime/multipart"
 	"net/http"
 	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -16,52 +17,72 @@ type Uploader struct {
 
 func (u *Uploader) UploadFile(filePath string) error {
 	file, err := os.Open(filePath)
-	defer file.Close()
-
 	if err != nil {
 		return fmt.Errorf("cannot open file: %v", err)
 	}
+	defer file.Close()
 
 	pr, pw := io.Pipe()
 	mw := multipart.NewWriter(pw)
 	contentType := mw.FormDataContentType()
 
+	errChan := make(chan error, 1)
+
 	go func() {
 		defer pw.Close()
 
-		part, err := mw.CreateFormFile("file", contentType)
+		part, err := mw.CreateFormFile("file", filepath.Base(filePath))
 		if err != nil {
-			_ = pw.CloseWithError(err)
+			errChan <- err
 			return
 		}
 
 		if _, err = io.Copy(part, file); err != nil {
-			_ = pw.CloseWithError(err)
+			errChan <- err
 			return
 		}
 
-		if err := mw.Close(); err != nil {
-			_ = pw.CloseWithError(err)
+		if err = mw.Close(); err != nil {
+			errChan <- err
 			return
 		}
+
+		errChan <- nil
 	}()
 
 	req, err := http.NewRequest("POST", u.URL, pr)
 	if err != nil {
 		return fmt.Errorf("cannot create request: %v", err)
 	}
+	req.Header.Set("Content-Type", contentType)
 
-	req.Header.Set("Content-Type", mw.FormDataContentType())
-
-	client := &http.Client{Timeout: time.Duration(u.Timeout) * time.Second}
+	client := &http.Client{
+		Timeout: time.Duration(u.Timeout) * time.Second,
+	}
 	resp, err := client.Do(req)
 	if err != nil {
+
+		select {
+		case writeErr := <-errChan:
+			if writeErr != nil {
+				return fmt.Errorf("upload failed (write: %v): %v", writeErr, err)
+			}
+		default:
+		}
 		return fmt.Errorf("upload failed: %v", err)
 	}
 	defer resp.Body.Close()
 
+	if writeErr := <-errChan; writeErr != nil {
+		return fmt.Errorf("failed to write multipart body: %v", writeErr)
+	}
+
 	body, _ := io.ReadAll(resp.Body)
 	fmt.Println("Server response:", string(body))
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return fmt.Errorf("server returned %d: %s", resp.StatusCode, string(body))
+	}
 
 	return nil
 }
